0c632072138fc269fbe020168aca19e8598453c7
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-12-03 14:22:20 -0700

    change home page to allow Admin access

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,13 +6,24 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <Link to='/main' >
+    <main className='main-page border'>
       <div className='home-logo column center center-x'>      
         <div className='app-image-container'>
           <img src={sqrl} />
         </div>
         <h1>SQUEAKR</h1>
       </div>  
-    </Link>
+
+      <div className=' buttons column center'>
+        <Link to='/main' >
+          <button>User</button>
+        </Link>
+        <Link to='/admin' >
+          <button>Admin</button>
+        </Link>
+      </div>
+      
+
+    </main>
   )
 }
