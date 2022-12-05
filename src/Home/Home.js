d6f6f0695752b62060bcc46a0ac18d73a493e57f
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-12-05 14:42:16 -0700

    adjust app.test.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import sqrl from '../images/SqueakerIcon.png'
 import '../App.css'
 import './Home.css'
 import { Link } from 'react-router-dom'
 
 export const Home = () => {
+  const [loggingIn, setLoggingIn] = useState(false)
+
+  const isLoggingIn = () => {
+    setLoggingIn(true)
+  }
+
+  const notLoggingIn = () => {
+    setLoggingIn(false)
+  }
+  const [newUser, setNewUser] = useState(false)
+
+  const isNewUser = () => {
+    setNewUser(true)
+  }
+
+  const notNewUser = () => {
+    setNewUser(false)
+  }
+
   return (
     <main className='main-page center'>
       <div className='home-logo column center center-x'>      
@@ -17,14 +36,47 @@ export const Home = () => {
       <div className='spacer'></div> 
       <div className='spacer'></div> 
 
-      <div className=' buttons column center'>
-        <Link to='/user' >
-          <button>User</button>
-        </Link>
-      <div className='spacer'></div> 
-        <Link to='/admin' >
-          <button>Admin</button>
-        </Link>
+        {!loggingIn && !newUser &&
+          <div className='column center distribute'>        
+              <button onClick={isLoggingIn} >Login</button>
+              <div className='spacer'></div> 
+              <button onClick={isNewUser} >I'm A New User</button>
+          </div>
+        }
+
+      <div className='buttons column center'>
+
+        {loggingIn && 
+        <div className='column center'>
+          <span>Enter Username:</span> 
+          <input type='text'></input>
+          <Link to='/user' >
+            <button onClick={notLoggingIn} >Submit</button>
+          </Link>
+        </div>  
+        }
+
+        
+        {newUser && 
+          <div className='column center'>
+            <span>Enter New Username:</span> 
+            <input type='text'></input>
+            <Link className='column center distribute' to='/user' >
+              <button onClick={notNewUser}>Create New User</button>
+            </Link>
+          </div>
+        }
+
+        <div className='spacer'></div> 
+
+        <div className='debug row distribute'>debug only:        
+          <Link to='/user' >
+            <button>User</button>
+          </Link>
+          <Link to='/admin' >
+            <button>Admin</button>
+          </Link>
+        </div>
       </div>
       
 
