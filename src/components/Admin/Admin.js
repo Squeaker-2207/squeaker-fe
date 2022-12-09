5fd8fb6cd03c1460818d2b0ef7baaa0e8f4b3714
Jedeo <jedeopastor@gmail.com>
2022-12-08 19:03:11 -0700

    Add: console log

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,6 +3,7 @@ import { GetReported } from "../../queries/getReported";
 import Navbar from "../Navigation/Navbar";
 import "../Navigation/Navbar";
 export const Admin = () => {
+  console.log(GetReported())
   return (
     <div>
       <Navbar />
