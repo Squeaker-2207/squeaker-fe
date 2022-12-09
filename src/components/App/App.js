44e60c6bee9f40e1049811952341c9fa159e7d7d
Jedeo <jedeopastor@gmail.com>
2022-12-09 15:42:53 -0700

    removed landing component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,31 @@
-import {useState} from 'react'
-import { Route, Routes } from 'react-router-dom'
-
-import { Error } from '../Error/Error'
-import { Home } from '../Home/Home'
-import { User } from '../User/User'
-import { Admin } from '../Admin/Admin'
-import CreateNewUser from '../CreateNewUserForm/CreateNewUserForm'
-import './App.css'
-
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Error } from "../Error/Error";
+import { Home } from "../Home/Home";
+import { User } from "../User/User";
+import { Admin } from "../Admin/Admin";
+import CreateNewUser from "../CreateNewUserForm/CreateNewUserForm";
+import "./App.css";
 
 const App = () => {
- const [isAdminTabClicked, SetIsAdminClicked] = useState(false)
-
- //const [getUserSelection, setUserSelection] = useState({loggingIn: false, newUser:false }) 
-
+  const [isAdminTabClicked, SetIsAdminClicked] = useState(false);
   return (
-    <main className='app column center'>
-
+    <main className="app column center">
       <Routes>
-        {/* <Route exact path='/' element={<LandingPage setUserSelection={setUserSelection}/>}/> */}
-
-        <Route path='/' element={<Home/>} />
-         <Route path='/admin' element={<Admin SetIsAdminClicked={SetIsAdminClicked}/>} />
-         <Route path='/create-account' element={<CreateNewUser/>} />
-         <Route path='/user/:userId' element={<User isAdminTabClicked={isAdminTabClicked}/>} />
-        <Route path='*' element={<Error />} />
-      </Routes> 
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/admin"
+          element={<Admin SetIsAdminClicked={SetIsAdminClicked} />}
+        />
+        <Route path="/create-account" element={<CreateNewUser />} />
+        <Route
+          path="/user/:userId"
+          element={<User isAdminTabClicked={isAdminTabClicked} />}
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
