9b84972d3c63671dcd2a0bb3942330fe8df1db4a
Colby Pearce <101376200+Crpearce@users.noreply.github.com>
2022-12-09 18:02:18 -0700

    Update User.js

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,37 +1,36 @@
-import React, { useContext, useEffect } from "react";
-import { UserContext } from "../../contexts/userContext";
-import sqrl from "../../images/SqueakerIcon.png";
+import React from "react";
+// useContext, useEffect 
+// import { UserContext } from "../../contexts/userContext";
+// import sqrl from "../../images/SqueakerIcon.png";
 import "./User.css";
 // import { Squeak } from "../Squeak/Squeak";
-import { NewSqueak } from "../NewSqueak/NewSqueak";
-import { GetSqueaks } from "../../queries/getSqueaks";
-import { useParams } from "react-router-dom";
-
-
-import { GetUser } from "../../queries/getUser";
+// import { NewSqueak } from "../NewSqueak/NewSqueak";
+// import { GetSqueaks } from "../../queries/getSqueaks";
+// import { useParams } from "react-router-dom";
+// import { GetUser } from "../../queries/getUser";
 import Navbar from "../Navigation/Navbar";
 
 export const User = ({ isAdminTabClicked }) => {
 
-  const [user, setUser] = useContext(UserContext);
+  // const [user, setUser] = useContext(UserContext);
 
-  const { userId } = useParams();
-  const { data: userById } = GetUser(userId);
-  const { isAdmin = false } = user || {};
-  const { loading, error, data } = GetSqueaks();
+  // const { userId } = useParams();
+  // const { data: userById } = GetUser(userId);
+  // const { isAdmin = false } = user || {};
+  // const { loading, error, data } = GetSqueaks();
 
-  useEffect(() => {
-    if (!user) {
-      setUser(userById);
-    }
-  }, [user, userById, setUser]);
+  // useEffect(() => {
+  //   if (!user) {
+  //     setUser(userById);
+  //   }
+  // }, [user, userById, setUser]);
 
 
-  // if (error) return <p>Error : {error.message}</p>;
-  // if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error : {error.message}</p>;
+//   if (loading) return <p>Loading...</p>;
 //  const displaySqueaks = data.allSqueaks.map((squeak) => {
-    return (
-    <div>hey</div>
+//     return (
+//     <div>hey</div>
 //      <Squeak
 //        id={squeak.id}
 //        content={squeak.content}
@@ -40,8 +39,8 @@ export const User = ({ isAdminTabClicked }) => {
 //        isAdminTabClicked={isAdminTabClicked}
 //        squeak={squeak}
 //     />
-    );
- // });
+//     );
+
 
   return (
     <main>
@@ -49,16 +48,16 @@ export const User = ({ isAdminTabClicked }) => {
       <header className="row center">
         <h1>SQUEAKR</h1>
         <div className="main-image-container">
-          <img src={sqrl} alt="alt text" />
+          {/* <img src={sqrl} alt="alt text" /> */}
         </div>
       </header>
 
-      <NewSqueak />
+      {/* <NewSqueak />
       <button>💬</button>
       <section className="main-content-squeaks column center">
      
-      </section>
+      </section> */}
     </main>
   );
 };
-
+// }
