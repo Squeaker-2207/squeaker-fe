1010c33a13f1bf209fde05752afd06b8cbdb40e8
Jedeo <jedeopastor@gmail.com>
2022-12-10 09:39:04 -0700

    Add client Colby

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { ApolloProvider } from "@apollo/client";
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
 import { LoginProvider } from '../../contexts/loginContext';
 import { UserProvider } from '../../contexts/userContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+
+const client = new ApolloClient({
+  uri: "https://squeakr-be.fly.dev/graphql/",
+  cache: new InMemoryCache(),
+});
 
 test('renders squeakr app', () => {
   render(
