0814c24c98bf586cfb548831a16f117585329dde
Jedeo <jedeopastor@gmail.com>
2022-12-12 12:22:06 -0700

    Add add report and add nut GQL

---
+++ src/Mutations/addReports.js
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const ADD_REPORT = gql`
+  mutation AddReport($id: ID!, $report: Boolean!) {
+    updateSqueak(input: { id: $id, report: $report}) {
+      squeak {
+        content
+        nuts
+      }
+    }
+  }
+`;
